db87ea53aa70af1160575244565ceade21a1b69d
Pablo Roberto <pablomoura2016@gmail.com>
2024-04-30 16:34:35 -0300

    logica de dificuldade

--- lib/main.dart
+++ lib/main.dart
@@ -25,24 +25,40 @@ class MyApp extends StatelessWidget {
           scrollDirection: Axis.vertical,
           //mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Task("Aprender Flutter",
-                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large"),
-            Task("Aprender bike",
-                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large"),
-            Task("Aprender programação",
-                "https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg"),
-            Task("Aprender a dançar",
-                "https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg"),
+            Task(
+                "Aprender Flutter",
+                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large",
+                3),
+            Task(
+                "Aprender bike",
+                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large",
+                2),
+            Task(
+                "Aprender programação",
+                "https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg",
+                5),
+            Task(
+                "Aprender a dançar",
+                "https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg",
+                4),
             Task("Aprender a tocar",
-                "https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg"),
-            Task("Aprender programação",
-                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large"),
-            Task("Aprender programação",
-                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large"),
-            Task("Aprender programação",
-                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large"),
-            Task("Aprender programação",
-                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large"),
+                "https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg", 1),
+            Task(
+                "Aprender programação",
+                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large",
+                1),
+            Task(
+                "Aprender programação",
+                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large",
+                4),
+            Task(
+                "Aprender programação",
+                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large",
+                3),
+            Task(
+                "Aprender programação",
+                "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large",
+                2),
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -59,8 +75,9 @@ class Task extends StatefulWidget {
   final String nomeTarefa;
 
   final String img;
+  final int dificuldade;
 
-  const Task(this.nomeTarefa, this.img, {super.key});
+  const Task(this.nomeTarefa, this.img, this.dificuldade, {super.key});
 
   @override
   State<Task> createState() => _TaskState();
@@ -97,15 +114,50 @@ class _TaskState extends State<Task> {
                           fit: BoxFit.cover,
                         ),
                       ),
-                      Container(
-                          width: 200,
-                          child: Text(
-                            widget.nomeTarefa,
-                            style: TextStyle(
-                              fontSize: 24,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          )),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                              width: 200,
+                              child: Text(
+                                widget.nomeTarefa,
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              )),
+                          Row(
+                            children: [
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.dificuldade >= 1)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.dificuldade >= 2)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.dificuldade >= 3)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.dificuldade >= 4)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.dificuldade >= 5)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                            ],
+                          ),
+                        ],
+                      ),
                       Container(
                         height: 50,
                         width: 70,
@@ -147,7 +199,9 @@ class _TaskState extends State<Task> {
                         width: 200,
                         child: LinearProgressIndicator(
                           color: Colors.white,
-                          value: nivel / 10,
+                          value: (widget.dificuldade > 0)
+                              ? (nivel / widget.dificuldade) / 10
+                              : 1,
                         ),
                       ),
                     ),
