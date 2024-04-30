e7979170b42af0932d8e469f85c7b862c3221385
Pablo Roberto <pablomoura2016@gmail.com>
2024-04-30 13:16:17 -0300

    implementei o scrool

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,72 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          title: Text('Flutter: Meus primeiros passos'),
+          title: Text('Tarefas'),
         ),
-        body: Container(),
-        floatingActionButton: FloatingActionButton(onPressed: (){}),
+        body: ListView(
+          scrollDirection: Axis.horizontal,
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Task("Aprender Flutter"),
+            Task("Aprender bike"),
+            Task("Aprender programação"),
+            Task("Aprender programação"),
+            Task("Aprender programação"),
+            Task("Aprender programação"),
+            Task("Aprender programação"),
+            Task("Aprender programação"),
+            Task("Aprender programação"),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.blue,
+          child: Icon(Icons.add, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nomeTarefa;
+  const Task(this.nomeTarefa,{super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                Text(nomeTarefa),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                    ),
+                    child: Icon(
+                      Icons.arrow_drop_up,
+                      color: Colors.white,
+                    ))
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
