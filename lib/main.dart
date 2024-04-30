2c18347353d8d9ad94a4ff3e2b4cd147a489f99c
Pablo Roberto <pablomoura2016@gmail.com>
2024-04-30 14:47:05 -0300

    inseri a barra de contagem e a contagem

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,14 @@ class MyApp extends StatelessWidget {
           title: Text('Tarefas'),
         ),
         body: ListView(
-          scrollDirection: Axis.horizontal,
+          scrollDirection: Axis.vertical,
           //mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Task("Aprender Flutter"),
             Task("Aprender bike"),
             Task("Aprender programação"),
-            Task("Aprender programação"),
-            Task("Aprender programação"),
+            Task("Aprender a dançar"),
+            Task("Aprender a tocar"),
             Task("Aprender programação"),
             Task("Aprender programação"),
             Task("Aprender programação"),
@@ -45,45 +45,94 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nomeTarefa;
-  const Task(this.nomeTarefa,{super.key});
+
+  const Task(this.nomeTarefa, {super.key});
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(
-            color: Colors.blue,
-            height: 140,
-          ),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Column(
               children: [
                 Container(
-                  color: Colors.black26,
-                  width: 72,
+                  color: Colors.white,
                   height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Container(
+                          width: 200,
+                          child: Text(
+                            widget.nomeTarefa,
+                            style: TextStyle(
+                              fontSize: 24,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          )),
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              nivel++;
+                            });
+                          },
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.blue),
+                          ),
+                          child: Icon(
+                            Icons.arrow_drop_up,
+                            color: Colors.white,
+                          ))
+                    ],
+                  ),
                 ),
-                Text(nomeTarefa),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.blue),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 200,
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: nivel / 10,
+                        ),
+                      ),
                     ),
-                    child: Icon(
-                      Icons.arrow_drop_up,
-                      color: Colors.white,
-                    ))
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Nivel $nivel",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
